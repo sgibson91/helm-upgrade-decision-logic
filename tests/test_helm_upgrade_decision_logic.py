cc59214db6fcc540df4a05bc56a7eb2f10c488f0
Sarah Gibson <drsarahlgibson@gmail.com>
2022-03-11 10:11:25 +0000

    Begin writing tests

---
+++ tests/test_helm_upgrade_decision_logic.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from helm_upgrade_decision_logic import generate_lists_of_filepaths_and_filenames
+
+
+def test_generate_lists_of_filepaths_and_filenames():
+    input_filepaths = [
+        "config/clusters/cluster_1/cluster.yaml",
+        "config/clusters/cluster_1/hub_1.values.yaml",
+        "config/clusters/cluster_1/support.values.yaml",
+    ]
+
+    # Expected returns
+    expected_cluster_filepaths = [Path("config/clusters/cluster_1")]
+    expected_cluster_files = {"config/clusters/cluster_1/cluster.yaml"}
+    expected_values_files = {"config/clusters/cluster_1/hub_1.values.yaml", "config/clusters/cluster_1/support.values.yaml"}
+    expected_support_files = {"config/clusters/cluster_1/support.values.yaml"}
+
+    target_cluster_filepaths, target_cluster_files, target_values_files, target_support_files = generate_lists_of_filepaths_and_filenames(input_filepaths)
+
+    assert target_cluster_filepaths == expected_cluster_filepaths
+    assert target_cluster_files == expected_cluster_files
+    assert target_values_files == expected_values_files
+    assert target_support_files == expected_support_files
+
+    assert isinstance(target_cluster_filepaths, list)
+    assert isinstance(target_cluster_files, set)
+    assert isinstance(target_values_files, set)
+    assert isinstance(target_support_files, set)
