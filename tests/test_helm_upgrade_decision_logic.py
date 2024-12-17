1f5a3d2b241bfe69867d1f1ee1e5f1663597a85b
Sarah Gibson <drsarahlgibson@gmail.com>
2024-12-17 13:00:54 +0000

    Update tests

--- tests/test_helm_upgrade_decision_logic.py
+++ tests/test_helm_upgrade_decision_logic.py
@@ -1,21 +1,21 @@
 import os
 from pathlib import Path
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from ruamel.yaml import YAML
 
 from mymodule.helm_upgrade_decision_logic import (
     assign_staging_jobs_for_missing_clusters,
     discover_modified_common_files,
-    ensure_support_staging_jobs_have_correct_keys,
     generate_hub_matrix_jobs,
     generate_support_matrix_jobs,
     get_all_cluster_yaml_files,
-    move_staging_jobs_to_staging_matrix,
 )
 
 yaml = YAML(typ="safe", pure=True)
 case = TestCase()
+root_path = Path(__file__).parent.parent
+clusters_path = root_path.joinpath("tests/test-clusters")
 
 
 def test_get_all_cluster_yaml_files():
@@ -24,7 +24,10 @@ def test_get_all_cluster_yaml_files():
         Path(os.getcwd()).joinpath("tests/test-clusters/cluster2/cluster.yaml"),
     }
 
-    result_cluster_files = get_all_cluster_yaml_files(test_env=True)
+    with mock.patch(
+        "mymodule.helm_upgrade_decision_logic.CONFIG_CLUSTERS_PATH", clusters_path
+    ):
+        result_cluster_files = get_all_cluster_yaml_files()
 
     assert result_cluster_files == expected_cluster_files
     assert isinstance(result_cluster_files, set)
@@ -208,8 +211,7 @@ def test_generate_support_matrix_jobs_one_cluster():
         {
             "provider": "gcp",
             "cluster_name": "cluster1",
-            "upgrade_support": "true",
-            "reason_for_support_redeploy": "Following helm chart values files were modified: support.values.yaml",
+            "reason_for_redeploy": "Following helm chart values files were modified: support.values.yaml",
         }
     ]
 
@@ -223,8 +225,7 @@ def test_generate_support_matrix_jobs_one_cluster():
 
     assert "provider" in result_matrix_jobs[0].keys()
     assert "cluster_name" in result_matrix_jobs[0].keys()
-    assert "upgrade_support" in result_matrix_jobs[0].keys()
-    assert "reason_for_support_redeploy" in result_matrix_jobs[0].keys()
+    assert "reason_for_redeploy" in result_matrix_jobs[0].keys()
 
 
 def test_generate_support_matrix_jobs_all_clusters():
@@ -252,8 +253,7 @@ def test_generate_support_matrix_jobs_all_clusters():
             {
                 "provider": "gcp",
                 "cluster_name": "cluster1",
-                "upgrade_support": "true",
-                "reason_for_support_redeploy": reason,
+                "reason_for_redeploy": reason,
             }
         ]
 
@@ -272,8 +272,7 @@ def test_generate_support_matrix_jobs_all_clusters():
 
         assert "provider" in result_matrix_jobs[0].keys()
         assert "cluster_name" in result_matrix_jobs[0].keys()
-        assert "upgrade_support" in result_matrix_jobs[0].keys()
-        assert "reason_for_support_redeploy" in result_matrix_jobs[0].keys()
+        assert "reason_for_redeploy" in result_matrix_jobs[0].keys()
 
 
 def test_discover_modified_common_files_hub_helm_charts():
@@ -306,182 +305,6 @@ def test_discover_modified_common_files_support_helm_chart():
     assert not upgrade_all_hubs
 
 
-def test_move_staging_jobs_to_staging_matrix_job_exists():
-    input_hub_matrix_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "hub_name": "staging",
-            "reason_for_redeploy": "cluster.yaml file was modified",
-        },
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "hub_name": "hub1",
-            "reason_for_redeploy": "cluster.yaml file was modified",
-        },
-    ]
-    input_support_staging_matrix_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "upgrade_support": "true",
-            "reason_for_support_redeploy": "cluster.yaml file was modified",
-        }
-    ]
-
-    expected_hub_matrix_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "hub_name": "hub1",
-            "reason_for_redeploy": "cluster.yaml file was modified",
-        },
-    ]
-    expected_support_staging_matrix_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "upgrade_support": "true",
-            "reason_for_support_redeploy": "cluster.yaml file was modified",
-            "upgrade_staging": "true",
-            "reason_for_staging_redeploy": "cluster.yaml file was modified",
-        }
-    ]
-
-    (
-        result_hub_matrix_jobs,
-        result_support_staging_matrix_jobs,
-    ) = move_staging_jobs_to_staging_matrix(
-        input_hub_matrix_jobs, input_support_staging_matrix_jobs
-    )
-
-    case.assertCountEqual(result_hub_matrix_jobs, expected_hub_matrix_jobs)
-    case.assertCountEqual(
-        result_support_staging_matrix_jobs, expected_support_staging_matrix_jobs
-    )
-
-    assert "upgrade_staging" in result_support_staging_matrix_jobs[0].keys()
-    assert "reason_for_staging_redeploy" in result_support_staging_matrix_jobs[0].keys()
-
-
-def test_move_staging_jobs_to_staging_matrix_job_does_not_exist():
-    input_hub_matrix_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "hub_name": "staging",
-            "reason_for_redeploy": "cluster.yaml file was modified",
-        },
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "hub_name": "hub1",
-            "reason_for_redeploy": "cluster.yaml file was modified",
-        },
-    ]
-    input_support_staging_matrix_jobs = []
-
-    expected_hub_matrix_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "hub_name": "hub1",
-            "reason_for_redeploy": "cluster.yaml file was modified",
-        },
-    ]
-    expected_support_staging_matrix_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "upgrade_support": "false",
-            "reason_for_support_redeploy": "",
-            "upgrade_staging": "true",
-            "reason_for_staging_redeploy": "cluster.yaml file was modified",
-        }
-    ]
-
-    (
-        result_hub_matrix_jobs,
-        result_support_staging_matrix_jobs,
-    ) = move_staging_jobs_to_staging_matrix(
-        input_hub_matrix_jobs, input_support_staging_matrix_jobs
-    )
-
-    case.assertCountEqual(result_hub_matrix_jobs, expected_hub_matrix_jobs)
-    case.assertCountEqual(
-        result_support_staging_matrix_jobs, expected_support_staging_matrix_jobs
-    )
-
-    assert "upgrade_staging" in result_support_staging_matrix_jobs[0].keys()
-    assert "reason_for_staging_redeploy" in result_support_staging_matrix_jobs[0].keys()
-
-
-def test_ensure_support_staging_jobs_have_correct_keys_hubs_exist():
-    input_support_staging_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "upgrade_support": "false",
-            "reason_for_support_upgrade": "",
-        }
-    ]
-
-    input_hub_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "hub_name": "hub1",
-            "reason_for_redeploy": "",
-        }
-    ]
-
-    expected_support_staging_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "upgrade_support": "false",
-            "reason_for_support_upgrade": "",
-            "upgrade_staging": "true",
-            "reason_for_staging_redeploy": "Following prod hubs require redeploy: hub1",
-        }
-    ]
-
-    result_support_staging_jobs = ensure_support_staging_jobs_have_correct_keys(
-        input_support_staging_jobs, input_hub_jobs
-    )
-
-    case.assertCountEqual(result_support_staging_jobs, expected_support_staging_jobs)
-
-
-def test_ensure_support_staging_jobs_have_correct_keys_hubs_dont_exist():
-    input_support_staging_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "upgrade_support": "false",
-            "reason_for_support_upgrade": "",
-        }
-    ]
-
-    expected_support_staging_jobs = [
-        {
-            "cluster_name": "cluster1",
-            "provider": "gcp",
-            "upgrade_support": "false",
-            "reason_for_support_upgrade": "",
-            "upgrade_staging": "false",
-            "reason_for_staging_redeploy": "",
-        }
-    ]
-
-    result_support_staging_jobs = ensure_support_staging_jobs_have_correct_keys(
-        input_support_staging_jobs, []
-    )
-
-    case.assertCountEqual(result_support_staging_jobs, expected_support_staging_jobs)
-
-
 def test_assign_staging_jobs_for_missing_clusters_is_missing():
     input_prod_jobs = [
         {
@@ -491,22 +314,23 @@ def test_assign_staging_jobs_for_missing_clusters_is_missing():
         },
     ]
 
-    expected_support_staging_jobs = [
+    expected_staging_jobs = [
         {
             "provider": "gcp",
             "cluster_name": "cluster1",
-            "upgrade_support": "false",
-            "reason_for_support_redeploy": "",
-            "upgrade_staging": "true",
-            "reason_for_staging_redeploy": "Following prod hubs require redeploy: hub1",
+            "hub_name": "staging",
+            "reason_for_redeploy": "Following prod hubs require redeploy: hub1",
         }
     ]
 
-    result_support_staging_jobs = assign_staging_jobs_for_missing_clusters(
-        [], input_prod_jobs
-    )
+    with mock.patch(
+        "mymodule.helm_upgrade_decision_logic.CONFIG_CLUSTERS_PATH", clusters_path
+    ):
+        result_staging_jobs = assign_staging_jobs_for_missing_clusters(
+            [], input_prod_jobs
+        )
 
-    case.assertCountEqual(result_support_staging_jobs, expected_support_staging_jobs)
+    case.assertCountEqual(result_staging_jobs, expected_staging_jobs)
 
 
 def test_assign_staging_jobs_for_missing_clusters_is_present():
@@ -518,30 +342,26 @@ def test_assign_staging_jobs_for_missing_clusters_is_present():
         },
     ]
 
-    input_support_staging_jobs = [
+    input_staging_jobs = [
         {
             "provider": "gcp",
             "cluster_name": "cluster1",
-            "upgrade_support": "false",
-            "reason_for_support_redeploy": "",
-            "upgrade_staging": "true",
-            "reason_for_staging_redeploy": "Following prod hubs require redeploy: hub1",
+            "hub_name": "staging",
+            "reason_for_redeploy": "Following prod hubs require redeploy: hub1",
         }
     ]
 
-    expected_support_staging_jobs = [
+    expected_staging_jobs = [
         {
             "provider": "gcp",
             "cluster_name": "cluster1",
-            "upgrade_support": "false",
-            "reason_for_support_redeploy": "",
-            "upgrade_staging": "true",
-            "reason_for_staging_redeploy": "Following prod hubs require redeploy: hub1",
+            "hub_name": "staging",
+            "reason_for_redeploy": "Following prod hubs require redeploy: hub1",
         }
     ]
 
-    result_support_staging_jobs = assign_staging_jobs_for_missing_clusters(
-        input_support_staging_jobs, input_prod_jobs
+    result_staging_jobs = assign_staging_jobs_for_missing_clusters(
+        input_staging_jobs, input_prod_jobs
     )
 
-    case.assertCountEqual(result_support_staging_jobs, expected_support_staging_jobs)
+    case.assertCountEqual(result_staging_jobs, expected_staging_jobs)
